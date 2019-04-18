204ed4fa41d9d4477cdc2459402606d09f994f34
xiangruili <xiangrui.li@gmail.com>
2019-04-18 09:13:40 -0400

    avoid error for output for 'save'
    Return useless empty in case required

--- nii_tool.m
+++ nii_tool.m
@@ -317,6 +317,7 @@ elseif strcmpi(cmd, 'save')
     end
     if isempty(fext), fext = '.nii'; end % default single .nii file
     fname = fullfile(pth, fname); % without file ext
+    if nargout, varargout{1} = []; end
     isNii = strcmpi(fext, '.nii'); % will use .img/.hdr if not .nii
     
     % Deal with NIfTI version and sizeof_hdr
