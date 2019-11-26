1c89134292691b5faa3241f7a4f9e0e65430e35d
xiangruili <xiangrui.li@gmail.com>
2019-11-26 16:51:17 -0500

    new resize scheme
    won't restrict width/height ratio, but keep axis ratio

--- nii_viewer.m
+++ nii_viewer.m
@@ -96,7 +96,7 @@ function varargout = nii_viewer(fname, varargin)
 % 
 % Background image and overlays are listed at the top-left of the panel. All
 % parameters of the bottom row of the panel are for the highlighted image. This
-% feature is indicated by the blue background of the parameters. Each NIfTI file
+% feature is indicated by the highlighted frame the parameters. Each NIfTI file
 % has its own set of parameters (display min and max value, LUT, alpha, whether
 % to smooth, interpolation method, and volume number) to control its display.
 % Moving the mouse onto a parameter will show the meaning of the parameter.
@@ -342,7 +342,7 @@ if numel(figNam)>40, figNam = [figNam(1:40) '...']; end
 figNam = ['nii_viewer - ' figNam ' (' formcode2str(hs.form_code(1)) ')'];
 set(fh, 'Toolbar', 'none', 'Menubar', 'none', 'Renderer', 'opengl', ...
     'NumberTitle', 'off', 'Tag', 'nii_viewer', 'DockControls', 'off', ...
-    'Position', [figPos siz+[0 64]], 'Name', figNam);
+    'Position', [figPos siz+[2 66]], 'Name', figNam);
 cb = @(cmd) {@nii_viewer_cb cmd hs.fig}; % callback shortcut
 xyz = [0 0 0]; % start cursor location
 c = round(p.Ri * [xyz 1]'); c = c(1:3)' + 1; % 
@@ -391,21 +391,21 @@ hs.params = ph;
 feature('DefaultCharacterSet', 'UTF-8'); % for old matlab to show triangles
 hs.overlay(1) = uicontrol(ph, 'Style', 'pushbutton', 'FontSize', 7, ...
     'Callback', cb('stack'), 'Enable', 'off', 'SelectionHighlight', 'off', ...
-    'String', char(9660), 'Position', [1 36 16 15], 'Tag', 'down', ...
+    'String', char(9660), 'Position', [1 37 16 15], 'Tag', 'down', ...
     'TooltipString', 'Move selected image one level down');
 hs.overlay(2) = copyobj(hs.overlay(1), ph);
 set(hs.overlay(2), 'Callback', cb('stack'), ...
     'String', char(9650), 'Position', [1 50 16 15], 'Tag', 'up', ...
     'TooltipString', 'Move selected image one level up');
 
-hs.value = uicontrol(ph, 'Style', 'text', 'Position', [208 38 pos(3)-208 20], ...
+hs.value = uicontrol(ph, 'Style', 'text', 'Position', [208 40 pos(3)-208 20], ...
     'BackgroundColor', clr, 'FontSize', 8+(~ispc && ~ismac), ...
     'TooltipString', '(x,y,z): top ... bottom');
 
 % IJK java spinners
 labls = 'IJK';
 str = {'Left to Right' 'Posterior to Anterior' 'Inferior to Superior'};
-pos = [38 44 22]; posTxt = [36 10 20];
+pos = [42 44 22]; posTxt = [40 10 20];
 for i = 1:3
     loc = [(i-1)*64+34 pos];
     txt = sprintf('%s, 1:%g', str{i}, dim(i));
@@ -415,52 +415,53 @@ for i = 1:3
 end
 
 % Controls for each file
-uipanel('Parent', ph, 'Units', 'pixels', 'Position', [1 2 412 34], ...
-    'BorderType', 'none', 'BorderWidth', 0, 'BackgroundColor', [0 0.5 0.8]);
-hs.lb = java_spinner([7 8 48 22], [p.lb -inf inf p.lb_step], ph, ...
+uipanel(ph, 'Units', 'pixels', 'Position', [1 4 412 33], ...
+    'BorderType', 'line', 'BorderWidth', 3, 'HighlightColor', [0 0.5 0.8]);
+hs.lb = java_spinner([7 10 48 22], [p.lb -inf inf p.lb_step], ph, ...
     cb('lb'), '#.##', 'min value (threshold)');
-hs.ub = java_spinner([59 8 56 22], [p.ub -inf inf p.ub_step], ph, ...
+hs.ub = java_spinner([59 10 56 22], [p.ub -inf inf p.ub_step], ph, ...
     cb('ub'), '#.##', 'max value (clipped)');
 hs.lutStr = {'grayscale' 'red' 'green' 'blue' 'violet' 'yellow' 'cyan' ...
     'red-yellow' 'blue-green' 'two-sided'  '<html><font color="red">lines' ...
     'parula' 'jet' 'hsv' 'hot' 'cool' 'spring' 'summer' 'autumn' 'winter' ...
     'bone' 'copper' 'pink' 'prism' 'flag' 'phase' 'phase3' 'phase6' 'RGB' 'custom'};
-hs.lut = uicontrol(ph, 'Style', 'popup', 'Position', [113 8 74 22], ...
+hs.lut = uicontrol(ph, 'Style', 'popup', 'Position', [113 10 74 22], ...
     'String', hs.lutStr, 'BackgroundColor', 'w', 'Callback', cb('lut'), ...
     'Value', p.lut, 'TooltipString', 'Lookup table options for non-RGB data');
 if p.lut==numel(hs.lutStr), set(hs.lut, 'Enable', 'off'); end
 
-hs.alpha = java_spinner([187 8 44 22], [1 0 1 0.1], ph, cb('alpha'), '#.#', ...
+hs.alpha = java_spinner([187 10 44 22], [1 0 1 0.1], ph, cb('alpha'), '#.#', ...
     'Alpha: 0 transparent, 1 opaque');
 
 hs.smooth = uicontrol(ph, 'Style', 'checkbox', 'Value', p.smooth, ...
-    'Position', [231 8 60 22], 'String', 'smooth', 'BackgroundColor', clr, ...
+    'Position', [231 10 60 22], 'String', 'smooth', 'BackgroundColor', clr, ...
     'Callback', cb('smooth'), 'TooltipString', 'Smooth image in 3D');
-hs.interp = uicontrol(ph, 'Style', 'popup', 'Position', [291 8 68 22], ...
+hs.interp = uicontrol(ph, 'Style', 'popup', 'Position', [291 10 68 22], ...
     'String', {'nearest' 'linear' 'cubic' 'spline'}, 'Value', p.interp, ...
     'Callback', cb('interp'), 'Enable', 'off', 'BackgroundColor', 'w', ... 
     'TooltipString', 'Interpolation method for overlay');
-hs.volume = java_spinner([361 8 44 22], [1 1 nVol 1], ph, cb('volume'), '#', ...
+hs.volume = java_spinner([361 10 44 22], [1 1 nVol 1], ph, cb('volume'), '#', ...
     ['Volume number, 1:' num2str(nVol)]);
 hs.volume.setEnabled(nVol>1);
 
 %% Three views: sag, cor, tra
 % this panel makes resize easy: axes relative to panel
-hs.frame = uipanel(fh, 'Units', 'pixels', 'Position', [1 1 siz], ...
+hs.frame = uipanel(fh, 'Units', 'pixels', 'Position', [2 2 siz], ...
     'BorderType', 'none', 'BackgroundColor', 'k');
 
 for i = 1:3
     j = 1:3; j(j==i) = [];
     hs.ax(i) = axes('Position', axPos(i,:), 'Parent', hs.frame);
     hs.hsI(i) = handle(image(zeros(dim(j([2 1])), 'single')));
+    set(hs.ax(i), 'DataAspectRatio', [1./hs.pixdim(j) 1]);
     hold(hs.ax(i), 'on');
     
     x = [c(j(1))+[-1 1 0 0]*hs.gap(j(1)); 0 dim(j(1))+1 c(j(1))*[1 1]];
     y = [c(j(2))+[0 0 -1 1]*hs.gap(j(2)); c(j(2))*[1 1] 0 dim(j(2))+1];
     hs.cross(i,:) = line(x, y);
 
-    hs.xyz(i) = text(0.02, 0.96, num2str(xyz(i)), 'Units', 'normalized', ...
-        'Parent', hs.ax(i), 'FontSize', 12);
+    hs.xyz(i) = text(hs.ax(i), 0.02, 0.96, num2str(xyz(i)), ...
+        'Units', 'normalized', 'FontSize', 12);
 end
 set(hs.hsI, 'ButtonDownFcn', cb('mousedown'));
 p.hsI = hs.hsI; % background img
@@ -548,9 +549,9 @@ for i = [1 1.2 1.5 2 3 4 5 8 10 20]
     uimenu(h, 'Label', num2str(i), 'Callback', cb('zoom'));
 end
 h = uimenu(h_view, 'Label', 'Layout', 'UserData', pf.layout);
-uimenu(h, 'Label', 'one-row', 'Callback', cb('layout'));
-uimenu(h, 'Label', 'two-row sag on right', 'Callback', cb('layout'));
-uimenu(h, 'Label', 'two-row sag on left', 'Callback', cb('layout'));
+uimenu(h, 'Label', 'one-row', 'Callback', cb('layout'), 'Tag', '1');
+uimenu(h, 'Label', 'two-row sag on right', 'Callback', cb('layout'), 'Tag', '2');
+uimenu(h, 'Label', 'two-row sag on left', 'Callback', cb('layout'), 'Tag', '3');
 uimenu(h_view, 'Label', 'White background', 'Callback', cb('background'));
 hLR = uimenu(h_view, 'Label', 'Right on left side', 'Callback', cb('flipLR'));
 uimenu(h_view, 'Label', 'Show colorbar', 'Callback', cb('colorbar'));
@@ -618,6 +619,8 @@ catch
 end
 try java_dnd(jFrame, cb('drop')); catch me, disp(me.message); end
 
+% iconPNG = fullfile(fileparts(mfilename('fullpath')), 'nii_viewer.png'); 
+% fh.JavaFrame.setFigureIcon(javax.swing.ImageIcon(iconPNG)); % windows only
 set(fh, 'ResizeFcn', cb('resize'), ... % 'SizeChangedFcn' for later matlab
     'WindowKeyPressFcn', @KeyPressFcn, 'CloseRequestFcn', cb('closeFig'), ...
     'PaperPositionMode', 'auto', 'InvertHardcopy', 'off', 'HandleVisibility', 'Callback');
@@ -708,43 +711,11 @@ switch cmd
         if strcmp(cmd, 'volume'), set_xyz(hs); end
         set_cdata(hs);
     case 'resize'
-        if isempty(hs), return; end
-        cb = fh.ResizeFcn;
-        fh.ResizeFcn = []; drawnow; % avoid weird effect
-        clnObj = onCleanup(@() set(fh, 'ResizeFcn', cb)); % restore func
-        
-        posP = getpixelposition(hs.panel); % get old height in pixels
-        posF = getpixelposition(fh); % asked position by user
-        posI = getpixelposition(hs.frame); % old size
-        
-        siz = hs.frame.Position(3:4);
-        res = screen_pixels(1);
-        oldF = round([posI(3) posI(4)+posP(4)]); % previous fig size
-        if isequal(oldF, posF(3:4)), return; end % moving without size change
-        if all(posF(3:4) >= oldF) % enlarge
-            a = max([posF(3) posF(4)-posP(4)] ./ siz) * siz;
-            a(1) = min(a(1), res(1)-30); % leave space for MAC dock etc
-            a(2) = min(a(2), res(2)-92-posP(4)); % leave space for title bar etc
-            a = min(a ./ siz) * siz;
-        elseif all(posF(3:4) <= oldF) % shrink
-            a = min([posF(3) posF(4)-posP(4)] ./ siz) * siz;
-        else % one side enlarge, another side shrink: use old size
-            a = posI(3:4);
-        end
-        d = posF(1)+a(1)-res(1);
-        if d>0, posF(1) = posF(1) - d; end
-        d = posF(2)+a(2)+posP(4)+92-res(2);
-        if d>0, posF(2) = posF(2) - d; end
-        posF(1) = max(posF(1), 10);
-        posF(2) = max(posF(2), 50);
-        posF(3:4) = [a(1) a(2)+posP(4)]; % final figure size
-        fh.Position = posF; % done for fig
-        
-        posP(2) = posF(4)-posP(4)+1; 
-        posP(3) = posF(3);
-        hs.panel.Position = posP; % done for control panel
-        hs.frame.Position = [1 1 a]; % done for image panel
-        
+        if isempty(hs), return; end        
+        htP = hs.panel.Position(4); % get old height in pixels
+        posF = getpixelposition(fh); % asked size by user
+        hs.panel.Position(2:3) = posF([4 3]) - [htP 1]; % control panel
+        hs.frame.Position(3:4) = posF(3:4) - [2 htP]; % image panel
         nii_viewer_cb([], [], 'width', fh);
     case 'toggle' % turn on/off NIfTI
         i = h.getAnchorSelectionIndex+1;
@@ -998,20 +969,16 @@ switch cmd
             set(hs.ras([3 5]), 'String', 'R');
         end
     case 'layout'
-        submenu = {'one-row' 'two-row sag on right' 'two-row sag on left'};
-        layout = find(strcmp(get(h, 'Label'), submenu));
-        if hs.pref.UserData.layout == layout, return; end
-        
-        cb = hs.fig.ResizeFcn;
-        hs.fig.ResizeFcn = ''; drawnow;
-        clnObj = onCleanup(@() set(hs.fig, 'ResizeFcn', cb));
-        
+        layout = str2double(get(h, 'Tag'));
+        parent = get(h, 'Parent');
+        if get(parent, 'UserData') == layout, return; end
+        set(parent, 'UserData', layout);
+        htP = hs.panel.Position(4);
         [siz, axPos, figPos] = plot_pos(hs.dim.*hs.pixdim, layout);
-        hs.fig.Position = [figPos siz+[0 64]];
+        hs.fig.Position = [figPos siz+[2 htP+2]];
         hs.frame.Position(3:4) = siz;
-        hs.panel.Position(2) =  hs.fig.Position(4) - 64;
+        hs.panel.Position(2:3) = [hs.fig.Position(4)-htP siz(1)+2];
         for i = 1:4, set(hs.ax(i), 'Position', axPos(i,:)); end
-        hs.pref.UserData.layout = layout;
     case 'keyHelp'
         str = sprintf([ ...
            'Key press available when focus is not in a number dialer:\n\n' ...
@@ -1142,7 +1109,7 @@ switch cmd
         dim = size(img);
         img = reshape(img, [], prod(dim(4:end)))';
         img = img(:, b(:));
-        fh1 = figure(mod(fh.Number,10)+jf);
+        fh1 = figure;
         if strcmp(labl, 'Time course')
             img = mean(single(img), 2);
         else
@@ -2057,7 +2024,7 @@ uicontrol(d, 'Style', 'text', 'Position', [8 306 300 22], ...
 hs.openPath = uicontrol(d, 'Style', 'edit', 'String', pf.openPath, ...
     'Position', [8 288 350 22], 'BackgroundColor', 'w', 'HorizontalAlignment', 'left', ...
     'TooltipString', 'nii_viewer will point to this folder when you "Open" image');
-uicontrol('Parent', d, 'Position', [358 289 30 22], 'Tag', 'browse', ...
+uicontrol(d, 'Position', [358 289 30 22], 'Tag', 'browse', ...
     'String', '...', 'Callback', @pref_dialog_cb);
 
 hs.rightOnLeft = uicontrol(d, 'Style', 'popup', 'BackgroundColor', 'w', ...
@@ -2087,12 +2054,12 @@ uipanel(d, 'Units', 'Pixels', 'Position', [4 110 390 56], 'BorderType', 'etchedi
     'BorderWidth', 2, 'Title', 'For "Save NIfTI as" if interpolation is applicable');
 str = {'nearest' 'linear' 'cubic' 'spline'};
 val = find(strcmp(str, pf.interp));
-uicontrol('Parent', d, 'Style', 'text', 'Position', [8 116 140 22], ...
+uicontrol(d, 'Style', 'text', 'Position', [8 116 140 22], ...
     'String', 'Interpolation method:', 'HorizontalAlignment', 'right');
 hs.interp = uicontrol(d, 'Style', 'popup', 'String', str, ...
     'Position', [150 120 68 22], 'Value', val, 'BackgroundColor', 'w');
 
-uicontrol('Parent', d, 'Style', 'text', 'Position', [230 116 90 22], ...
+uicontrol(d, 'Style', 'text', 'Position', [230 116 90 22], ...
     'String', 'Missing value:', 'HorizontalAlignment', 'right');
 hs.extraV = uicontrol(d, 'Style', 'edit', 'String', num2str(pf.extraV), ...
     'Position', [324 120 60 22], 'BackgroundColor', 'w', ...
@@ -2102,15 +2069,15 @@ str = strtrim(cellstr(num2str([0 120 150 200 300 600 1200]')));
 val = find(strcmp(str, pf.dpi));
 uipanel(d, 'Units', 'Pixels', 'Position', [4 40 390 56], 'BorderType', 'etchedin', ...
     'BorderWidth', 2, 'Title', 'For "Save figure as" and "Copy figure"');
-uicontrol('Parent', d, 'Style', 'text', 'Position', [8 46 90 22], ...
+uicontrol(d, 'Style', 'text', 'Position', [8 46 90 22], ...
     'String', 'Resolution:', 'HorizontalAlignment', 'right');
 hs.dpi = uicontrol(d, 'Style', 'popup', 'String', str, ...
     'Position', [110 50 50 22], 'Value', val, 'BackgroundColor', 'w', ...
     'TooltipString', 'in DPI (0 means screen resolution)');
 
-uicontrol('Parent', d, 'Position', [300 10 70 24], 'Tag', 'OK', ...
+uicontrol(d, 'Position', [300 10 70 24], 'Tag', 'OK', ...
     'String', 'OK', 'Callback', @pref_dialog_cb);
-uicontrol('Parent', d, 'Position',[200 10 70 24], ...
+uicontrol(d, 'Position',[200 10 70 24], ...
     'String', 'Cancel', 'Callback', 'delete(gcf)');
 
 set(d, 'Position', pos, 'Visible', 'on');
@@ -2132,16 +2099,14 @@ if strcmp(get(h, 'Tag'), 'OK') % done
         set(fh, 'WindowButtonMotionFcn', '');        
     end
     
-    i = get(hs.interp, 'Value');
     str = get(hs.interp, 'String');
-    pf.interp = str{i};
+    pf.interp = str{get(hs.interp, 'Value')};
     
     pf.extraV = str2double(get(hs.extraV, 'String'));
     pf.openPath = get(hs.openPath, 'String');
 
-    i = get(hs.dpi, 'Value');
     str = get(hs.dpi, 'String');
-    pf.dpi = str{i};
+    pf.dpi = str{get(hs.dpi, 'Value')};
     
     hs1 = guidata(fh);
     set(hs1.pref, 'UserData', pf);
@@ -2667,7 +2632,7 @@ catch % restore reoriented img
 end
 
 if ~isempty(strfind(c, 'a copy')) %#ok<*STREMP> % a copy
-    [fname, pName] = uiputfile([pName '/*.nii'], 'Input name for FSL RGB file');
+    [fname, pName] = uiputfile([pName '/*.nii'], 'Input file name');
     if ~ischar(fname), return; end
     fname = fullfile(pName, fname);
     nii_tool('save', nii, fname);
@@ -3245,7 +3210,7 @@ fh = hsN.fig.UserData;
 if isempty(fh) || ~ishandle(fh) % create surface figure
     fh = figure(hsN.fig.Number+100);
     set(fh, 'NumberTitle', 'off', 'MenuBar', 'none', 'Renderer', 'opengl', ...
-        'Color', 'k', 'HandleVisibility', 'Callback', 'InvertHardcopy', 'off');
+        'HandleVisibility', 'Callback', 'InvertHardcopy', 'off');
     if isnumeric(fh), fh = handle(fh); end
 
     cMenu = uicontextmenu('Parent', fh);
@@ -3268,12 +3233,13 @@ if isempty(fh) || ~ishandle(fh) % create surface figure
     r = 0.96; % width of two columns, remaining for colorbar
     pos = [0 1 r 1; 0 0 r 1; r 1 r 1; r 0 r 1] / 2;
     gii = cii2nii(); % get buffered Anatomical gii
+    hs.frame = uipanel(fh, 'Position', [0 0 1 1], 'BackgroundColor', 'k');
     for ig = 1:2
         v = gii.Vertices{ig};
         lim = [min(v)' max(v)'];
         im = ones(size(v,1), 3, 'single') * 0.667;
         for i = ig*2+[-1 0]
-            hs.ax(i) = axes('Parent', fh, 'Position', pos(i,:), 'CameraViewAngle', 6.8);
+            hs.ax(i) = axes('Parent', hs.frame, 'Position', pos(i,:), 'CameraViewAngle', 6.8);
             axis vis3d; axis equal; axis off;
             set(hs.ax(i), 'XLim', lim(1,:), 'YLim', lim(2,:), 'ZLim', lim(3,:));
             hs.patch(i) = patch('Parent', hs.ax(i), 'EdgeColor', 'none', ...
@@ -3284,7 +3250,7 @@ if isempty(fh) || ~ishandle(fh) % create surface figure
     end
     set(hs.patch, 'ButtonDownFcn', {@cii_view_cb 'buttonDownPatch'}, 'UIContextMenu', cMenu);
     
-    hs.ax(5) = axes('Position', [r 0.1 1-r 0.8], 'Visible', 'off', 'Parent', fh);
+    hs.ax(5) = axes('Position', [r 0.1 1-r 0.8], 'Visible', 'off', 'Parent', hs.frame);
     try
         hs.colorbar = colorbar(hs.ax(5), 'PickableParts', 'none');
     catch % for early matlab
@@ -3293,12 +3259,11 @@ if isempty(fh) || ~ishandle(fh) % create surface figure
     end
     set(hs.colorbar, 'Location', 'East', 'Visible', get(hsN.colorbar, 'Visible'));
             
-    fh.Position(3:4) = [1/r diff(lim(3,:))/diff(lim(2,:))] * 600;
+    fh.Position(3:4) = [1/r diff(lim(3,:))/diff(lim(2,:))] * 600 + 4;
     srn = get(0, 'MonitorPositions');
     dz = srn(1,4)- 60 - sum(fh.Position([2 4]));
     if dz<0, fh.Position(2) = fh.Position(2) + dz; end
 
-    fh.ResizeFcn           = {@cii_view_cb 'resizeG'};
     fh.WindowButtonUpFcn   = {@cii_view_cb 'buttonUp'};
     fh.WindowButtonDownFcn = {@cii_view_cb 'buttonDown'};
     fh.WindowButtonMotionFcn={@cii_view_cb 'buttonMotion'};
@@ -3406,17 +3371,6 @@ switch cmd
             set(hs.patch, 'VertexNormalsMode', 'auto');
             drawnow; set(hs.patch, 'VertexNormalsMode', 'manual');
         end
-    case 'resizeG'
-        sz = getpixelposition(h); % asked position
-        cb = hs.fig.ResizeFcn;
-        hs.fig.ResizeFcn = []; drawnow; % avoid weird effect
-        r = get(hs.ax(5), 'Position');
-        r = diff(get(hs.ax(1), 'ZLim')) / diff(get(hs.ax(1), 'YLim')) * r(1);
-        if sz(3)/sz(4) < r, sz = sz(3) * [1 r];
-        else, sz = sz(4) * [1/r 1];
-        end
-        hs.fig.Position(3:4) = sz;
-        hs.fig.ResizeFcn = cb;
     case 'zoomG'
         va = get(hs.ax(1), 'CameraViewAngle'); 
         if strcmp(get(h, 'Label'), 'Zoom in'), va = va * 0.9;
@@ -3454,7 +3408,7 @@ switch cmd
         hs.fig.Name = ['cii_view - ' mName ' (' frm ')'];
     case 'background'
         clr = hs.hsN.frame.BackgroundColor;
-        hs.fig.Color = clr;
+        set(hs.frame, 'BackgroundColor', clr);
         set(hs.colorbar, 'EdgeColor', 1-clr);
     case 'colorbar' % colorbar on/off
         set(hs.colorbar, 'Visible', get(hs.hsN.colorbar, 'Visible'));
